217a51c26ce76892260e358261fd9f8fa841e1cb
gpantov <gpantov@codix.bg>
2023-04-05 21:49:13 +0300

    Add New Object Functionality   - Create New Object   - Add Manuel Fields

--- Dynamic/src/ pages/add/add.module.ts
+++ Dynamic/src/ pages/add/add.module.ts
@@ -1,14 +1,39 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {AddComponent} from './add.component';
+import {ButtonModule} from 'primeng/button';
+import {DialogModule} from 'primeng/dialog';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {InputTextModule} from 'primeng/inputtext';
+import {FormsModule} from '@angular/forms';
+import {DropdownModule} from 'primeng/dropdown';
+import {CardModule} from 'primeng/card';
+import {CalendarModule} from 'primeng/calendar';
+import {InputNumberModule} from 'primeng/inputnumber';
+import {ToggleButtonModule} from 'primeng/togglebutton';
+import {DividerModule} from 'primeng/divider';
 
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    ButtonModule,
+    DialogModule,
+    BrowserAnimationsModule,
+    InputTextModule,
+    FormsModule,
+    DropdownModule,
+    CardModule,
+    CalendarModule,
+    InputNumberModule,
+    ToggleButtonModule,
+    DividerModule
+  ],
+  exports: [
+    AddComponent,
   ],
   declarations: [
-    AddComponent
+    AddComponent,
   ]
 })
 export class AddModule {}
--- Dynamic/src/app/app.module.ts
+++ Dynamic/src/app/app.module.ts
@@ -3,15 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {AddModule} from '../ pages/add/add.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        AddModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
